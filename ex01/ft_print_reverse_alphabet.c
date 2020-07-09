d321d84862a9aa5f479f9f7e4a4fde41d89b4177
Ntokozo Maduna <ntmaduna@c4r9s5.wethinkcode.co.za>
2020-07-09 10:52:24 +0200

    ex01

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntmaduna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/09 10:40:08 by ntmaduna          #+#    #+#             */
+/*   Updated: 2020/07/09 10:44:08 by ntmaduna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char alphabet;
+
+	alphabet = 'z';
+
+	while(alphabet >= 'a')
+	{
+		ft_putchar(alphabet);
+		alphabet = alphabet - 1;
+	}
+}
