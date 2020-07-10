fd595cf716ebe77a7847c3325bc0bf47ada9b23d
Ntokozo Maduna <ntmaduna@c4r9s5.wethinkcode.co.za>
2020-07-10 09:28:47 +0200

    ex04

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntmaduna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/09 12:28:03 by ntmaduna          #+#    #+#             */
+/*   Updated: 2020/07/10 09:25:23 by ntmaduna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_print_comb(void)
+{
+	char numb1;
+	char numb2;
+	char numb3;
+
+	numb1 = '0';
+	while(numb1 <= '7')
+	{
+		numb2 = '1';
+		while(numb2 <= '8')
+		{
+			numb3 = '2';
+			while(numb3 <= '9')
+			{
+				ft_putchar(numb1);
+				ft_putchar(numb2);
+				ft_putchar(numb3);
+				ft_putchar(',');
+				ft_putchar(' ');
+				numb3++;
+			}
+			numb2++;
+		}
+		numb1++;
+	}
+}
+
